a04aff1b9188f5d75d37578be1e1ee38f5186c43
1995rishi@gmail.com <1995rishi@gmail.com>
2018-05-13 15:33:38 +0530

    e

--- code.py
+++ code.py
@@ -9,6 +9,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import SGDClassifier
 from nltk.corpus import stopwords
+
+from sklearn.cluster import MeanShift, estimate_bandwidth
+
 #import tensorflow as tf
 
 # np.random.seed(1337)
@@ -39,7 +42,7 @@ with open('train.csv','r') as training_file:
 	row_count = 0
 	try:
 		for row in trainCSV:
-			if row_count >= 20000:
+			if row_count >= 5000:
 				break
 			if row_count != 0:
 				data = preprocess(row[1]) + preprocess(row[2])
@@ -120,67 +123,94 @@ test_feats = tfidf_transformer.transform(test_counts)
 
 
 ################################################################ SVM ########################################################
-print "Starting SVM ....."
-classifierModel = svm.SVC(probability=True, verbose=False, decision_function_shape='ovr')
-classifierModel.fit(train_feats, updated_train_owner)
-predict_prob = classifierModel.predict_proba(test_feats)
-classes = classifierModel.classes_ 
-# print predict
-print "classes = ",len(classes)
-k=int(0.05*len(classes))
-match = 0
-for j,prob in enumerate(predict_prob):
-	expected = updated_test_owner[j]
-	prob = [ [i,prob[i]] for i in range(len(prob))]
-	prob = sorted(prob, reverse = True, key = lambda x: x[1])
-	for i in range(k):
-		c = prob[i][0]
-		if classes[c]==expected:
-			match+=1
-			break
-print "accuracy = ", float(match)/float(len(predict_prob))*100
+# print "Starting SVM ....."
+# classifierModel = svm.SVC(probability=True, verbose=False, decision_function_shape='ovr')
+# classifierModel.fit(train_feats, updated_train_owner)
+# predict_prob = classifierModel.predict_proba(test_feats)
+# classes = classifierModel.classes_ 
+# # print predict
+# print "classes = ",len(classes)
+# k=int(0.05*len(classes))
+# match = 0
+# for j,prob in enumerate(predict_prob):
+# 	expected = updated_test_owner[j]
+# 	prob = [ [i,prob[i]] for i in range(len(prob))]
+# 	prob = sorted(prob, reverse = True, key = lambda x: x[1])
+# 	for i in range(k):
+# 		c = prob[i][0]
+# 		if classes[c]==expected:
+# 			match+=1
+# 			break
+# print "accuracy = ", float(match)/float(len(predict_prob))*100
 
 
 
 ##############################################################  Naive Bayes #################################################
-print "Starting Naive Bayes....."
-classifierModel = MultinomialNB(alpha=0.01)        
-classifierModel = OneVsRestClassifier(classifierModel).fit(train_feats, updated_train_owner)
-predict = classifierModel.predict_proba(test_feats)
-classes = classifierModel.classes_
-print "classes = ",len(classes)
-k=int(0.05*len(classes))
-match = 0
-for j,prob in enumerate(predict):
-	expected = updated_test_owner[j]
-	prob = [ [i,prob[i]] for i in range(len(prob))]
-	prob = sorted(prob, reverse = True, key = lambda x: x[1])
+# print "Starting Naive Bayes....."
+# classifierModel = MultinomialNB(alpha=0.01)        
+# classifierModel = OneVsRestClassifier(classifierModel).fit(train_feats, updated_train_owner)
+# predict = classifierModel.predict_proba(test_feats)
+# classes = classifierModel.classes_
+# print "classes = ",len(classes)
+# k=int(0.05*len(classes))
+# match = 0
+# for j,prob in enumerate(predict):
+# 	expected = updated_test_owner[j]
+# 	prob = [ [i,prob[i]] for i in range(len(prob))]
+# 	prob = sorted(prob, reverse = True, key = lambda x: x[1])
 	
-	for i in range(k):
-		c = prob[i][0]
-		if classes[c]==expected:
-			match+=1
-			break
-print "accuracy = ", float(match)/float(len(predict))*100
+# 	for i in range(k):
+# 		c = prob[i][0]
+# 		if classes[c]==expected:
+# 			match+=1
+# 			break
+# print "accuracy = ", float(match)/float(len(predict))*100
 
 
 #############################################################  SGD Classification ###########################################
-print "Starting SGD....."
-classifierModel = SGDClassifier(loss='log', alpha=0.01)
-classifierModel.fit(train_feats, updated_train_owner)
-predict_prob = classifierModel.predict_proba(test_feats)
-classes = classifierModel.classes_ 
-# print predict
-print "classes = ",len(classes)
-k=int(0.05*len(classes))
-match = 0
-for j,prob in enumerate(predict_prob):
-	expected = updated_test_owner[j]
-	prob = [ [i,prob[i]] for i in range(len(prob))]
-	prob = sorted(prob, reverse = True, key = lambda x: x[1])
-	for i in range(k):
-		c = prob[i][0]
-		if classes[c]==expected:
-			match+=1
-			break
-print "accuracy = ", float(match)/float(len(predict_prob))*100
+# print "Starting SGD....."
+# classifierModel = SGDClassifier(loss='log', alpha=0.01)
+# classifierModel.fit(train_feats, updated_train_owner)
+# predict_prob = classifierModel.predict_proba(test_feats)
+# classes = classifierModel.classes_ 
+# # print predict
+# print "classes = ",len(classes)
+# k=int(0.05*len(classes))
+# match = 0
+# for j,prob in enumerate(predict_prob):
+# 	expected = updated_test_owner[j]
+# 	prob = [ [i,prob[i]] for i in range(len(prob))]
+# 	prob = sorted(prob, reverse = True, key = lambda x: x[1])
+# 	for i in range(k):
+# 		c = prob[i][0]
+# 		if classes[c]==expected:
+# 			match+=1
+# 			break
+# print "accuracy = ", float(match)/float(len(predict_prob))*100
+
+
+############################################################   MeanShift Clustering #########################################
+print "Starting Meanshift Clustering...."
+bandwidth = estimate_bandwidth(train_feats.toarray(), quantile=0.2, n_samples=500)
+print "estimated bandwidth = ", bandwidth
+# ms = MeanShift(bandwidth=bandwidth)
+# ms = ms.fit(train_feats.toarray())
+# predict = ms.predict(test_feats)
+# labels = ms.labels_
+# cluster_devs = {}
+# for i in range(len(labels)):
+# 	try:
+# 		cluster_devs[labels[i]].append(updated_train_owner[i])
+# 	except:
+# 		cluster_devs[labels[i]] = [updated_train_owner[i]]
+
+# match = 0
+# print cluster_devs
+# for i, label in enumerate(predict):
+# 	expected = updated_test_owner[i]
+# 	if expected in cluster_devs[label]:
+# 		match += 1
+
+# print "accuracy = ", float(match)/float(len(predict))*100
+# cluster_centers = ms.cluster_centers_
+# print len(cluster_centers)
